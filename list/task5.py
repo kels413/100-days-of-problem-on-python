78ae91c04349121a134541e14cd4781066d0bd68
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 03:20:30 +0100

    solved the last question

---
+++ list/task5.py
@@ -0,0 +1,19 @@
+"""
+List Concatenation:
+Create a function that takes two lists as input and returns a new list that is the concatenation of the two input lists. For example, if the input lists are [1, 2, 3] and [4, 5, 6], the output should be [1, 2, 3, 4, 5, 6].
+"""
+
+# function signature
+
+def two_list(lst1=None, lst2=None):
+    if lst1 is None or not isinstance(lst1, list):
+        raise ValueError("Input must be a list")
+    
+    sum = lst1 + lst2
+    return sum
+
+try:
+    concatnate = two_list([1,2,3,4,5], [6,7,8,9,10])
+    print(concatnate)
+except ValueError as ve:
+    print(f"Error: {ve}")
