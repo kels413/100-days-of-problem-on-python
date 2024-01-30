9939041903b10f361f534a033d1c05005b93fde6
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 03:05:08 +0100

    completed task4

--- list/task4.py
+++ list/task4.py
@@ -2,3 +2,21 @@
 List Square:
 Write a program that takes a list of numbers as input and returns a new list where each element is the square of the corresponding element in the original list.
 """
+
+def square_list(lst=None):
+
+    if lst is None or not isinstance(lst, list):
+        raise ValueError("Input must be a list")
+
+    square_lst = []
+
+    for i in lst:
+        square_lst.append(i * i)
+    return square_lst
+
+try:
+    content = square_list([1,2,3,4,5])
+    # content = square_list("dewfe")
+    print(content)
+except ValueError as ve:
+    print(f"Error: {ve}")
