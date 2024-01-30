2f201f354d5e71040b4c53ca9150127deeefd0d1
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 03:41:03 +0100

    trying to figure out the try block is not catching the execption

--- list/task5.py
+++ list/task5.py
@@ -9,11 +9,15 @@ def two_list(lst1=None, lst2=None):
     if lst1 is None or not isinstance(lst1, list):
         raise ValueError("Input must be a list")
     
-    sum = lst1 + lst2
-    return sum
+    if lst2 is None or not isinstance(lst2, list):
+        raise ValueError("Input must be a list")
+    
+    result = lst1 + lst2
+    return result
 
 try:
-    concatnate = two_list([1,2,3,4,5], [6,7,8,9,10])
+    # concatnate = two_list([1,2,3,4,5], [6,7,8,9,10])
+    concatnate = two_list([1,2,3,4,5], ["efawf"])
     print(concatnate)
 except ValueError as ve:
     print(f"Error: {ve}")
