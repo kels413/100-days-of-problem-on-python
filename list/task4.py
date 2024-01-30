f7bdc5b5bc50e6bfb5c98f44d44b670415b2af20
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 02:38:59 +0100

    a List that takes a list of Number as input an returns a list square

---
+++ list/task4.py
@@ -0,0 +1,4 @@
+"""
+List Square:
+Write a program that takes a list of numbers as input and returns a new list where each element is the square of the corresponding element in the original list.
+"""
