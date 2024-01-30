4e98a67d3f85610b34815d3046c8d425315dd21c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 02:18:31 +0100

    handled Exception

--- list/task2.py
+++ list/task2.py
@@ -5,9 +5,16 @@ Write a program that reverses a given list. For example, if the input is [1, 2, 
 
 def reverse_list(lst=None):
     reversed_lst = []
-    for i in reversed(lst):
-        reversed_lst.append(i)
-    return reversed_lst
+    if lst is not None and isinstance(lst, list):
+        for i in reversed(lst):
+            reversed_lst.append(i)
+        return reversed_lst
+    else:
+        raise ValueError("sorry only list is allowed")
       
-content = reverse_list([1,2,3,4,5])
-print(content)
+try:
+    # content = reverse_list([1,2,3,4,5])
+    content = reverse_list("kelly")
+    print(content)
+except  ValueError as ve:
+    print(f"Error: {ve}")
