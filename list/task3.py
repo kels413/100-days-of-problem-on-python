6805eb3f3844a005000c9fd17a5c79d880c3952d
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 02:36:08 +0100

    Remember to handle edge cases (a string in the list)

--- list/task3.py
+++ list/task3.py
@@ -4,6 +4,9 @@ Create a function that takes a list of numbers and returns a new list containing
 """
 
 def even_list(lst=None):
+    if lst is None or not isinstance(lst, list):
+        raise ValueError("Input must be a list")
+    
     even_lst = []
 
     for i in lst:
@@ -11,5 +14,14 @@ def even_list(lst=None):
             even_lst.append(i)
     return even_lst
 
-content = even_list([1,2,3,4,5,6,7,8,9,10])
-print(content)
+try:
+    content = even_list([1,2,3,4,5,6,7,8,9,10])
+    # content = even_list("devae")
+    print(content)
+except ValueError as ve:
+    print(f"Error: {ve}")
+
+
+# case to check later
+    # a case where the user should input a string in
+    # between the list or any datatype that is not integer
